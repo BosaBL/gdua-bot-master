e85a01e17ddf997adf28f1093f8ad0c81c178974
BosaBL <christopher172003@outlook.cl>
2018-09-13 08:30:52 -0300

    asd33

--- comandos/del.js
+++ comandos/del.js
@@ -4,15 +4,14 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
   
   if(!args[0]) return message.channel.send("Error, necesito un cantidad.").then(msg => msg.delete(5000) + message.delete());
-  if(args[0] > 100) return message.channel.send(`**${message.author} me limititaron a borrar solamente 100 mensajes ;(**`).then(msg => msg.delete(5000));
-    message.delete()
+  if(args[0] > 100) return message.channel.send(`**${message.author} me limititaron a borrar solamente 100 mensajes ;(**`).then(msg => msg.delete(5000) + message.delete());
     message.channel.bulkDelete(args[0]).then(() => {
       message.channel.send( 
         { embed:{
           title: '**Borrando mensajes**',
           description: `${args[0]} mensajes eliminados`
         }
-      }).then(msg => msg.delete(5000) + msg.react("👌"));
+      }).then(msg => msg.delete(5000) + msg.react("👌") + message.delete());
     });
 }
 module.exports.help = {
